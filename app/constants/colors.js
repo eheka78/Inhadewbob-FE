f1a55ebfd53ddd28e98c234d9fdc86ecb55b6490
eheka78 <gemddkim22@gmail.com>
2025-11-22 00:08:05 +0900

    style: 룰렛색상 추가

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -4,3 +4,9 @@ export const colors = {
 	graphSubColor: "#E5E5E5",
 	text: "#000000",
 };
+
+export const RouletteColors = {
+	main: "#FFB5BE",	// 메인 컬러
+	sub: "#FFEBEE",	// 보조 및 패널 컬러
+	btnBorder: "#FF808F",	// 버튼 및 border 컬러
+}
