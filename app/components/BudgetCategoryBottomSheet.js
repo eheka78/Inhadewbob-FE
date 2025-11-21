c99c991c2d1c60b76822d67d2c62613be8bb8fe0
eheka78 <gemddkim22@gmail.com>
2025-11-22 00:54:03 +0900

    style: 카테고리, 전체

--- app/components/BudgetCategoryBottomSheet.js
+++ app/components/BudgetCategoryBottomSheet.js
@@ -1,8 +1,9 @@
-import { Pressable, StyleSheet, Text, TextInput, View, Button } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 import { FoodCategory } from './../../assets/FoodCategory';
+import { colors } from "../constants/colors";
 
 
-export default function BudgetCategoryBottomSheet({ setSelectedBudget, checked, setChecked }) {
+export default function BudgetCategoryBottomSheet({ selectedBudget, setSelectedBudget, checked, setChecked }) {
     // 체크했을 때, 체크하는 함수
     const toggle = (id) => {
         setChecked((prev) =>
@@ -14,50 +15,77 @@ export default function BudgetCategoryBottomSheet({ setSelectedBudget, checked, 
 
 
     return (
-        <View style={{ padding: 16 }}>
-            <Text style={styles.title}>오늘의 예산과 카테고리 설정해듀</Text>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 50 : 20}    // 키보드 나올 때 고려해서 offset 만듦.
+        >
+            <ScrollView
+                contentContainerStyle={{ paddingBottom: 10 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <View style={{ padding: 30 }}>
+                    <Text style={styles.title}>오늘의 예산 · 카테고리</Text>
 
-            {/* 예산 */}
-            <View style={{ marginVertical: 12 }}>
-                <Text>한 끼 예산</Text>
-                <TextInput
-                    placeholder="₩ 55,000"
-                    style={styles.input}
-                    onChange={setSelectedBudget}
-                />
-            </View>
+                    {/* 예산 */}
+                    <View style={{ marginVertical: 12 }}>
+                        <Text style={styles.subTitle}>한 끼 예산</Text>
+                        <TextInput
+                            keyboardType="number-pad"
+                            placeholder="가격을 입력하세요. (예: 8000)"
+                            style={styles.input}
+                            onChangeText={text => {
+                                // 숫자만 저장
+                                const numericText = text.replace(/[^0-9]/g, "");
+                                setSelectedBudget(numericText);
+                            }}
+                        />
 
-            {/* 카테고리 */}
-            <View>
-                <Text style={styles.subTitle}>카테고리</Text>
-                <View style={styles.categoryContainer}>
-                    {FoodCategory.map((item) => {
-                        const isChecked = checked.includes(item.id);
-                        return (
-                            <Pressable
-                                key={item.id}
-                                style={[
-                                    styles.tag,
-                                    isChecked && styles.tagSelected
-                                ]}
-                                onPress={() => toggle(item.id)}
-                            >
-                                <Text style={[
-                                    styles.tagText,
-                                    isChecked && styles.tagSelectedText
-                                ]}>
-                                    #{item.label}
-                                </Text>
-                            </Pressable>
-                        );
-                    })}
+                    </View>
+
+                    <View style={{padding: 10}}></View>
+
+                    {/* 음식 카테고리 */}
+                    <View>
+                        <Text style={styles.subTitle}>카테고리</Text>
+                        <View style={styles.tagContainer}>
+                            {FoodCategory.map((item) => {
+                                const isChecked = checked.includes(item.id);
+                                return (
+                                    <Pressable
+                                        key={item.id}
+                                        style={[
+                                            styles.tag,
+                                            isChecked && styles.tagSelected
+                                        ]}
+                                        onPress={() => toggle(item.id)}
+                                    >
+                                        <Text style={[
+                                            styles.tagText,
+                                            isChecked && styles.tagSelectedText
+                                        ]}>
+                                            #{item.label}
+                                        </Text>
+                                    </Pressable>
+                                );
+                            })}
+                        </View>
+                    </View>
+
+                    <Pressable
+                        style={{
+                            width: "100%",
+                            backgroundColor: colors.primary,
+                            borderRadius: 10,
+                            paddingVertical: 13,
+                            marginTop: 30
+                        }}
+                    >
+                        <Text style={{ color: "white", fontWeight: "30", fontSize: 20, textAlign: "center" }}>설정 완료</Text>
+                    </Pressable>
                 </View>
-            </View>
-
-            <View>
-                <Button title="설정" />
-            </View>
-        </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -66,7 +94,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontWeight: "600",
-        marginBottom: 16
+        marginBottom: 16,
+        textAlign:"center"
     },
     subTitle: {
         fontSize: 16,
@@ -80,23 +109,27 @@ const styles = StyleSheet.create({
         padding: 10,
         marginTop: 8,
     },
-    categoryContainer: {
+    tagContainer: {
         flexDirection: "row",
         flexWrap: "wrap",
-        gap: 8
+        gap: 8,
+        justifyContent: "center"
     },
     tag: {
-        paddingVertical: 6,
-        paddingHorizontal: 14,
-        borderRadius: 20,
-        backgroundColor: "#F0F0F0",
-    },
-    tagSelected: {
-        backgroundColor: "#5860ffff",
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderRadius: 30,
+        backgroundColor: "white",
+        borderColor: "black",
+        borderWidth: 1,
     },
     tagText: {
         fontSize: 15,
         color: "#333"
+    },
+    tagSelected: {
+        backgroundColor: colors.primary,
+        borderWidth: 0,
     },
     tagSelectedText: {
         color: "#fff",
