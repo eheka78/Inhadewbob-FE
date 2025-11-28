386f104b9434bf163d7b4118f20a3c6c4b06dfea
eheka78 <gemddkim22@gmail.com>
2025-11-28 15:15:45 +0900

    feat: 팔레트 색상 변경

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,12 +1,13 @@
 export const colors = {
 	background: "#FFFFFF",
-	primary: "#FF6B6B",  //#218FCA
-	graphSubColor: "#E5E5E5",
+	primary: "#5A8EF6",	// blue 2
+	primary2: "#F9EC8B",	// yellow 1
+	graphSubColor: "#ECF2FE",
 	text: "#000000",
 };
 
 export const RouletteColors = {
-	main: "#FFB5BE",	// 메인 컬러
-	sub: "#FFEBEE",	// 보조 및 패널 컬러
-	btnBorder: "#FF808F",	// 버튼 및 border 컬러
+	main: "#8BB1F8",	// blue 1  // 메인 컬러
+	sub: "#ECF2FE",	// blue 0  // 보조 및 패널 컬러  
+	btnBorder: "#5A8EF6",	// blue 2 	// 버튼 및 border 컬러
 }
