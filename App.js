db89fb528e075d128fc4f7381065a3aadb55d6f1
eheka78 <gemddkim22@gmail.com>
2025-11-30 01:11:50 +0900

    feat: 시작하면 frontPage로 이동하게

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -14,36 +14,56 @@ import FrontPage from "./app/screens/FrontPage";
 const Stack = createNativeStackNavigator();
 
 
+// LaunchScreen: 최상위에서 실행 체크
+function LaunchScreen({ navigation }) {
+    useEffect(() => {
+        console.log("APP START!");
+        navigation.replace("FrontPage");
+    }, []);
+
+    
+    // 로딩 화면
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <ActivityIndicator size="large" />
+        </View>
+    );
+}
+
 
 export default function App() {
+
     return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <SafeAreaProvider>
                 <NavigationContainer>
-                    <Stack.Navigator initialRouteName="Main">
+                    <Stack.Navigator
+                        screenOptions={{ headerShown: false }}
+                        initialRouteName="Launch"
+                    >
+                        <Stack.Screen
+                            name="Launch"
+                            component={LaunchScreen}
+                        />
                         <Stack.Screen
                             name="Main"
                             component={BottomTab}
-                            options={{headerShown: false}}
                         />
                         <Stack.Screen
-                            options={{ headerShown: false }}
                             name="FrontPage"
                             component={FrontPage}
                         />
                         <Stack.Screen
-                            options={{ headerShown: false }}
                             name="Login"
                             component={Login}
                         />
                         <Stack.Screen
-                            options={{ headerShown: false }}
                             name="InitialSetting"
                             component={InitialSetting}
                         />
                     </Stack.Navigator>
                 </NavigationContainer>
             </SafeAreaProvider>
-    </GestureHandlerRootView>
+        </GestureHandlerRootView>
     );
 }
