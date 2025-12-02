51cf1fb6f018c296e39c0451af85c742366b9a61
eheka78 <gemddkim22@gmail.com>
2025-12-02 18:38:17 +0900

    feat: 추천 예산 출력하기 위한 파라미터 전달

--- app/screens/Roulette.js
+++ app/screens/Roulette.js
@@ -72,6 +72,7 @@ export default function Roulette() {
                                     setSelectedBudget={setSelectedBudget}
                                     checked={checked}
                                     setChecked={setChecked}
+                                    recBudget={recBudget}
                                 />
                             </View>
                         </BottomSheetView>
