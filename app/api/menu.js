f75854964eadead68725a2fe9607df543983ba4c
eheka78 <gemddkim22@gmail.com>
2025-12-02 18:31:23 +0900

    feat: menu api 연결

--- app/api/menu.js
+++ app/api/menu.js
@@ -1,9 +1,13 @@
+import axios from "axios";
+import { loadAccessToken } from "../../tokenStorage";
+
+const BACKEND_URL = "https://inha-dewbob.p-e.kr";
+
+
 // 랜덤 식당 메뉴 조회(룰렛)
-export const getMenusByRoulette = async (date, category, price) => {
-    if (!date) {
-        console.log("getMenusByRoulette: date를 입력하세요.");
-        return;
-    }
+export const getMenusByRoulette = async (category, price) => {
+    const date = new Date().toISOString().split("T")[0];
+    const loadAccessTokened = await loadAccessToken();
 
     const paramsData = {};
 
@@ -12,11 +16,16 @@ export const getMenusByRoulette = async (date, category, price) => {
     if (price) { paramsData.price = price; }
 
     try {
-        const res = await api.get(`/menus/roulette`, {
+        const res = await axios.get(`${BACKEND_URL}/menus/roulette`, {
             params: { paramsData },
+            headers: {
+                Authorization: `Bearer ${loadAccessTokened}`,
+            },
         });
 
         console.log("getMenusByRoulette 조회 성공: ", res.data);
+        
+        return res.data;
 
     } catch (e) {
         console.error("getMenusByRoulette 실패: ", e);
@@ -25,18 +34,21 @@ export const getMenusByRoulette = async (date, category, price) => {
 
 
 // 추천 예산 조회
-export const getRecommendPrice = async (date) => {
-    if (!date) {
-        console.log("getRecommendPrice: date를 입력하세요.");
-        return;
-    }
+export const getRecommendPrice = async () => {
+    const date = new Date().toISOString().split("T")[0];
+    const loadAccessTokened = await loadAccessToken();
 
     try {
-        const res = await api.get('/menus/recom', {
+        const res = await axios.get(`${BACKEND_URL}/menus/recom`, {
             params: { date: date },
+            headers: {
+                Authorization: `Bearer ${loadAccessTokened}`,
+            },
         });
 
-        console.log("getRecommendPrice 조회 성공: ", res.data);
+        console.log("getRecommendPrice 조회 성공: ", res.data.recommendPrice);
+        
+        return res.data.recommendPrice;
 
     } catch (e) {
         console.error("getRecommendPrice 실패: ", e);
