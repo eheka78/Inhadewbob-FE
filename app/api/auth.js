8ab08052332f817a73e11483aa26cbbdc19bc90b
eheka78 <gemddkim22@gmail.com>
2025-12-02 18:16:21 +0900

    feat: auth api

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,117 +1,25 @@
 import axios from "axios";
+import { loadAccessToken } from "../../tokenStorage";
 
-// ✅ 순수 HTTP API 래퍼
-export const loginApi = (studentId, password) =>
-  axios.post("https://lost-inha.kro.kr/auth/login", {
-    studentId,
-    password,
-    isWeb: false,
-  });
-export const logoutApi = (studentId) => api.post("/auth/logout", { studentId });
-export const fetchMyInfoApi = () => api.get("/auth/profile");
+const BACKEND_URL = "https://inha-dewbob.p-e.kr";
 
-/*
-export async function loginApi(studentId, password) {
-  if (!studentId && !password) {
-    alert("studentId와 password를 입력하세요.");
-    return;
-  } else {
-    if (!studentId) {
-      alert("studentId를 입력하세요.");
-      return;
-    }
-    if (!password) {
-      alert("password를 입력하세요.");
-      return;
-    }
-  }
 
-  return axios.post("https://lost-inha.kro.kr/auth/login", {
-    studentId,
-    password,
-    isWeb: false,
-  });
-}
+// 소비 현황 수정
+export const getProfile = async () => {
+	const loadAccessTokened = await loadAccessToken();
+	if(loadAccessTokened == null) {	return null; }	
+	
+	try {
+		const res = await axios.get(`${BACKEND_URL}/auth/profile`, {
+			headers: {
+				Authorization: `Bearer ${loadAccessTokened}`,
+			},
+		});
+		console.log("Profile:", res.data);
 
-export async function fetchMyInfo() {
-  return api.get("/auth/profile");
-}
+		return res.data;
 
-export async function logoutApi() {
-  return api.post("/auth/logout");
-}
-
-/*
-export const login = async (studentId, password, navigate) => {
-  console.log("login start");
-  console.log("studentId: " + studentId + " password: " + password);
-
-  if (!studentId && !password) {
-    alert("studentId와 password를 입력하세요.");
-    return;
-  } else {
-    if (!studentId) {
-      alert("studentId를 입력하세요.");
-      return;
-    }
-    if (!password) {
-      alert("password를 입력하세요.");
-      return;
-    }
-  }
-
-  try {
-    const res = await axios.post("https://lost-inha.kro.kr/auth/login", {
-      studentId,
-      password,
-      isWeb: false,
-    });
-
-    TokenStore.setToken(res.data.accessToken);
-
-    // console.log(res.data);
-    console.log("로그인 성공");
-
-    // profile();
-
-    navigate.navigate("MainScreen");
-  } catch (err) {
-    console.error("에러 발생: ", err);
-    alert("로그인 실패");
-  }
+	} catch (e) {
+		console.error("Profile fetch error:", e);
+	}
 };
-*/
-
-/*
-export const logout = async (navigate) => {
-  console.log("logout start");
-
-  try {
-    await api.post("/auth/logout");
-
-    TokenStore.clearToken();
-
-    alert("로그아웃 성공");
-    console.log("로그아웃 성공");
-
-    navigate("/login");
-  } catch (err) {
-    console.error("에러 발생:", err);
-    alert("로그아웃 실패");
-  }
-};
-*/
-
-/*
-// 토큰을 통한 회원 정보 조회
-export async function profile() {
-  console.log("profile start");
-  try {
-    const res = await api.get("/auth/profile");
-    console.log("회원 정보 조회: " + res.data);
-    return res.data;
-  } catch (err) {
-    console.error("에러 발생:", err);
-  }
-}
-  */
