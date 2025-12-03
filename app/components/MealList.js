6d846ea3f8fe2e0b3e2069a2f9ad8fd373e3d1de
eheka78 <gemddkim22@gmail.com>
2025-12-03 18:54:15 +0900

    fix: 구조 수정으로 리스트 삭제 -> 리스트 파라미터로 받아옴

--- app/components/MealList.js
+++ app/components/MealList.js
@@ -3,17 +3,10 @@ import React from "react";
 import MealItem from "./MealItem";
 
 
-const dayMealList = [
-    { id: 1, store: "Rolling Pasta", menu: "봉골레 파스타", price: "6000", category: "양식", t: "08:30" },
-    { id: 2, store: "KFC", menu: "징거세트", price: "7900", category: "패스트푸드", t: "13:30" },
-    { id: 3, store: "Rolling Pasta", menu: "봉골레 파스타", price: "6000", category: "양식", t: "20:30" },
-];
-
-
-export default function MealList() {
+export default function MealList({ dayMealList }) {
     return (
         <View>
-            {dayMealList.map((item) => {
+            {dayMealList.length > 0 && dayMealList.map((item) => {
                 return (
                     <MealItem key={item.id} item={item} />
                 );
